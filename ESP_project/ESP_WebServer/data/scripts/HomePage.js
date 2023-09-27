867a5df9813190e375b8c154981aa3a277100383
Ksar2911 <p.goraj0@gmail.com>
2023-09-27 18:45:08 +0200

    HomePage - time & date done

--- ESP_project/ESP_WebServer/data/scripts/HomePage.js
+++ ESP_project/ESP_WebServer/data/scripts/HomePage.js
@@ -1,3 +1,21 @@
+// ------------- GET CURRENT TIME ---------\\
+export const time = () => {
+  const now = new Date();
+  const hours = now.getHours() < 10 ? `0${now.getHours()}` : now.getHours();
+  const minutes = now.getMinutes() < 10 ? `0${now.getMinutes()}` : now.getMinutes();
+  const seconds = now.getSeconds() < 10 ? `0${now.getSeconds()}` : now.getSeconds();
+
+  const year = now.getFullYear();
+  const month = now.getMonth() + 1 < 10 ? `0${now.getMonth() + 1}` : now.getMonth() + 1; // getMonth returns month 0 to 11
+  const day = now.getDate() < 10 ? `0${now.getDate()}` : now.getDate();
+
+  return {
+    time: `${hours}:${minutes}:${seconds}`,
+    date: `${day}.${month}.${year}`,
+  };
+};
+
+// ------------- RQNDOM QUOTE ---------\\
 const response = await fetch('../data/JSONs/quotes.json');
 const quotes = await response.json();
 
--- ESP_project/ESP_WebServer/data/scripts/app.js
+++ ESP_project/ESP_WebServer/data/scripts/app.js
@@ -1,9 +1,13 @@
-import { randomQuote } from './HomePage.js';
+import { time, randomQuote } from './HomePage.js';
 
 //---------------- NAV OPTIONS -----------------\\
 function loadContent(section) {
   $('#main-content').load(section);
 }
+
+const clockReload = () => {
+  $('.time__hour').text(time().time);
+};
 
 const quoteShow = () => {
   setTimeout(() => {
@@ -14,25 +18,34 @@ const quoteShow = () => {
 
 $(document).ready(function () {
   quoteShow();
+  setInterval(() => {
+    clockReload();
+  }, 1000);
+  $('.time__date').text(time().date);
 
   $('#main-btn').click(function () {
     loadContent('contents/home-content.html');
+    $('.mobile-logo').css('display', 'flex');
     quoteShow();
   });
 
   $('#power-btn').click(function () {
     loadContent('contents/power-content.html');
+    $('.mobile-logo').css('display', 'none');
   });
 
   $('#cooling-btn').click(function () {
     loadContent('contents/cooling-content.html');
+    $('.mobile-logo').css('display', 'none');
   });
 
   $('#analysys-btn').click(function () {
     loadContent('contents/analysys-content.html');
+    $('.mobile-logo').css('display', 'none');
   });
 
   $('#settings-btn').click(function () {
     loadContent('contents/settings-content.html');
+    $('.mobile-logo').css('display', 'none');
   });
 });
